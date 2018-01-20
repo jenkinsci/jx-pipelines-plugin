eca2d0df4412756a58b1fd84aed49b44d04639eb
James Strachan <james.strachan@gmail.com>
2018-01-20 12:33:13 +0000

    fix up use of git credentials

--- src/main/resources/org/jenkinsci/plugins/jx/pipelines/dsl/CommonFunctions.groovy
+++ src/main/resources/org/jenkinsci/plugins/jx/pipelines/dsl/CommonFunctions.groovy
@@ -100,6 +100,9 @@ class CommonFunctions {
     script.sh "git config user.name jenkins-x-bot"
     // TODO disable if no .gitcredentials
     script.sh "git config credential.https://github.com.username jenkins-x-bot"
+    script.sh "git config credential.helper store"
+    script.sh 'cp /root/gitcreds/gitcredentials ~/.git-credentials'
+    script.sh 'chmod 600  ~/.git-credentials'
 
 /*
     if (script.fileExists("root/.ssh-git")) {
